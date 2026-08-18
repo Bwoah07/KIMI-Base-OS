a399baf109e084d88319e462aacb6e505f40bf04
Bwoah <ggdccqzpbm@privaterelay.appleid.com>
2026-08-18 15:10:20 +0200

    Register Network Plug client screen

---
+++ mods/kimi-network-plug/src/main/java/com/bwoah07/kiminetworkplug/KimiNetworkPlugClient.java
@@ -0,0 +1,17 @@
+package com.bwoah07.kiminetworkplug;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+
+@Mod(value = KimiNetworkPlug.MODID, dist = Dist.CLIENT)
+public final class KimiNetworkPlugClient {
+    public KimiNetworkPlugClient(IEventBus modEventBus) {
+        modEventBus.addListener(this::registerScreens);
+    }
+
+    private void registerScreens(RegisterMenuScreensEvent event) {
+        event.register(KimiNetworkPlug.NETWORK_PLUG_MENU.get(), NetworkPlugScreen::new);
+    }
+}
