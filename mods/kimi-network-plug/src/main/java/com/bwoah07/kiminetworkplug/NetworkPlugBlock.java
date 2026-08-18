901adba53e1e5b73a94f0a1d60c179296e90be20
Bwoah <ggdccqzpbm@privaterelay.appleid.com>
2026-08-18 14:34:44 +0200

    Make Network Plugs self chunk-load

--- mods/kimi-network-plug/src/main/java/com/bwoah07/kiminetworkplug/NetworkPlugBlock.java
+++ mods/kimi-network-plug/src/main/java/com/bwoah07/kiminetworkplug/NetworkPlugBlock.java
@@ -40,6 +40,22 @@ public final class NetworkPlugBlock extends Block implements EntityBlock {
     }
 
     @Override
+    protected void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean movedByPiston) {
+        super.onPlace(state, level, pos, oldState, movedByPiston);
+        if (!level.isClientSide && !oldState.is(this) && level instanceof ServerLevel serverLevel) {
+            ChunkLoadingSavedData.register(serverLevel, pos);
+        }
+    }
+
+    @Override
+    protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
+        if (!level.isClientSide && !newState.is(this) && level instanceof ServerLevel serverLevel) {
+            ChunkLoadingSavedData.unregister(serverLevel, pos);
+        }
+        super.onRemove(state, level, pos, newState, movedByPiston);
+    }
+
+    @Override
     protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
         if (!level.isClientSide) {
             PlugMode next = state.getValue(MODE).next();
@@ -50,7 +66,7 @@ public final class NetworkPlugBlock extends Block implements EntityBlock {
                     : 0L;
 
             player.displayClientMessage(Component.literal(
-                    "Network Plug: " + next.name() + " | network " + networkEnergy + " / " + PowerNetworkSavedData.CAPACITY + " FE"
+                    "Network Plug: " + next.name() + " | chunk loaded | network " + networkEnergy + " / " + PowerNetworkSavedData.CAPACITY + " FE"
             ), true);
         }
 
