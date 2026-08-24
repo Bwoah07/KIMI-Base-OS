e4ef35b8186568ad56300fc970571a82f9aefe1b
Bwoah <ggdccqzpbm@privaterelay.appleid.com>
2026-08-24 16:19:44 +0200

    alpha46: harden door Lua unpack compatibility

--- modules/doors.lua
+++ modules/doors.lua
@@ -1,2 +1,6 @@
 -- Door feature contract: "pulse" "toggle" "invert" setAnalogOutput setEnabled setActive getInput getAnalogInput feedbackSide signal=
+-- CC:Tweaked environments differ on whether unpack lives globally or in table.
+if type(unpack) ~= "function" and type(table) == "table" and type(table.unpack) == "function" then
+    _G.unpack = table.unpack
+end
 return require("core.doors_impl")
