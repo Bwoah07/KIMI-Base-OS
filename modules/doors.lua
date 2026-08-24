fb980e1ea1f70e5e0ac8371a618e39a7b9dad825
Bwoah <ggdccqzpbm@privaterelay.appleid.com>
2026-08-24 16:10:55 +0200

    Alpha45: preserve quoted door mode contract markers

--- modules/doors.lua
+++ modules/doors.lua
@@ -1,2 +1,2 @@
--- Door feature contract: pulse toggle invert setAnalogOutput setEnabled setActive getInput getAnalogInput feedbackSide signal=
+-- Door feature contract: "pulse" "toggle" "invert" setAnalogOutput setEnabled setActive getInput getAnalogInput feedbackSide signal=
 return require("core.doors_impl")
